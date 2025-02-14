51ec01ee70f9fe17ab9c3c5a804b6180d77b63ac
Sebastián Cettour <sebas.cettour83@gmail.com>
2025-02-13 23:42:38 -0300

    mejoras esteticas

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ function App() {
         </div>
       </header>
 
-      <section className="bg-lime-500 py-20 px-5">
+      <section className="bg-lime-300 py-20 px-5">
         <div className="max-w-4xl mx-auto">
           <Form dispatch={dispatch} state={state} />
         </div>
